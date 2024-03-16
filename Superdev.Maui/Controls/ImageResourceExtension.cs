c3f55823e9715df732dc1a9aa2576868c6846da8
Thomas Galliker <thomas@bluewin.ch>
2024-03-16 17:53:58 +0100

    Add image resource extension

---
+++ Superdev.Maui/Controls/ImageResourceExtension.cs
@@ -0,0 +1,32 @@
+﻿using System.Reflection;
+
+namespace Superdev.Maui.Controls
+{
+    [ContentProperty("Source")]
+    public class ImageResourceExtension : IMarkupExtension
+    {
+        public string Source { get; set; }
+
+        private static Assembly targetAssembly;
+
+        /// <summary>
+        /// Use <seealso cref="Init"/> to define which assembly contains the resources which are resolved using <seealso cref="ImageResourceExtension"/>.
+        /// </summary>
+        /// <param name="resourceAssembly"></param>
+        public static void Init(Assembly resourceAssembly)
+        {
+            targetAssembly = resourceAssembly;
+        }
+
+        public object ProvideValue(IServiceProvider serviceProvider)
+        {
+            if (this.Source == null)
+            {
+                return null;
+            }
+
+            var imageSource = ImageSource.FromResource(this.Source, targetAssembly);
+            return imageSource;
+        }
+    }
+}
