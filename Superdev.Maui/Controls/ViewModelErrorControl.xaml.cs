a6636220eb9065642208235b7426648d430eb94c
Thomas Galliker <thomas@bluewin.ch>
2024-03-19 22:05:38 +0100

    Add ViewModelErrorControl

---
+++ Superdev.Maui/Controls/ViewModelErrorControl.xaml.cs
@@ -0,0 +1,50 @@
+﻿using Superdev.Maui.Mvvm;
+
+namespace Superdev.Maui.Controls
+{
+    public partial class ViewModelErrorControl : ContentView
+    {
+        public ViewModelErrorControl()
+        {
+            this.InitializeComponent();
+        }
+
+        public static readonly BindableProperty ViewModelErrorProperty = BindableProperty.Create(
+                nameof(ViewModelError),
+                typeof(ViewModelError),
+                typeof(ViewModelErrorControl),
+                ViewModelError.None);
+
+        public ViewModelError ViewModelError
+        {
+            get => (ViewModelError)this.GetValue(ViewModelErrorProperty);
+            set => this.SetValue(ViewModelErrorProperty, value);
+        }
+
+        public static readonly BindableProperty ImageSourceProperty = BindableProperty.Create(
+            nameof(ImageSource),
+            typeof(ImageSource),
+            typeof(ViewModelErrorControl),
+            ImageSource.FromResource("Superdev.Maui.Resources.Images.reload_192x192.png"));
+
+        public ImageSource ImageSource
+        {
+            get => (ImageSource)this.GetValue(ImageSourceProperty);
+            set => this.SetValue(ImageSourceProperty, value);
+        }
+
+        public static readonly BindableProperty LabelStyleProperty =
+            BindableProperty.Create(
+                nameof(LabelStyle),
+                typeof(Style),
+                typeof(ViewModelErrorControl),
+                default(Style),
+                BindingMode.OneWay);
+
+        public Style LabelStyle
+        {
+            get => (Style)this.GetValue(LabelStyleProperty);
+            set => this.SetValue(LabelStyleProperty, value);
+        }
+    }
+}
