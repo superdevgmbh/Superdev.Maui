38614a56e2bf7c976976cf3237bf04d25b2894ab
Thomas Galliker <>
2025-04-28 12:37:57 +0200

    Internals visible to unit tests

--- Superdev.Maui/Properties/AssemblyInfo.cs
+++ Superdev.Maui/Properties/AssemblyInfo.cs
@@ -1,6 +1,12 @@
-﻿[assembly: Superdev.Maui.Internals.Preserve(AllMembers = true)]
+﻿using System.Runtime.CompilerServices;
+using XmlnsPrefix = Microsoft.Maui.Controls.XmlnsPrefixAttribute;
 
-[assembly: Microsoft.Maui.Controls.XmlnsPrefix("http://schemas.superdev.ch/dotnet/2021/maui", "s")]
+[assembly: InternalsVisibleTo("Superdev.Maui.Tests")]
+
+[assembly: Superdev.Maui.Internals.Preserve(AllMembers = true)]
+
+[assembly: XmlnsPrefix("http://schemas.superdev.ch/dotnet/2021/maui", "s")]
+
 [assembly: XmlnsDefinition("http://schemas.superdev.ch/dotnet/2021/maui", "Superdev.Maui")]
 [assembly: XmlnsDefinition("http://schemas.superdev.ch/dotnet/2021/maui", "Superdev.Maui.Behaviors")]
 [assembly: XmlnsDefinition("http://schemas.superdev.ch/dotnet/2021/maui", "Superdev.Maui.Controls")]
