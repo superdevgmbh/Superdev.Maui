b2d7760eb5055d0f1728c7dadad3670923deeb50
Thomas Galliker <>
2025-04-28 11:37:27 +0200

    Cleanup

--- Superdev.Maui/Behaviors/EntryUnfocusedBehavior.cs
+++ Superdev.Maui/Behaviors/EntryUnfocusedBehavior.cs
@@ -82,15 +82,4 @@ namespace Superdev.Maui.Behaviors
             }
         }
     }
-
-    [Flags]
-    public enum TextDecorationFlags
-    {
-        None = 0,
-        TrimStart = 1,
-        TrimEnd = 2,
-        Trim = TrimStart | TrimEnd,
-        TrimWhitespaces = 4,
-        All = Trim | TrimWhitespaces,
-    }
 }
