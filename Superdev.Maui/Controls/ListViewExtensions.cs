2e998df70be7efcefe493a554d82984410ae1f28
Thomas Galliker <>
2025-05-02 09:45:03 +0200

    Add ListViewExtensions

---
+++ Superdev.Maui/Controls/ListViewExtensions.cs
@@ -0,0 +1,33 @@
+﻿namespace Superdev.Maui.Controls
+{
+    public class ListViewExtensions
+    {
+        public static BindableProperty ScrollToProperty =
+            BindableProperty.CreateAttached(
+                "ScrollTo",
+                typeof(object),
+                typeof(ListViewExtensions),
+                null,
+                propertyChanged: OnScrollToPropertyChanged);
+
+        public static object GetScrollTo(BindableObject bindable)
+        {
+            return bindable.GetValue(ScrollToProperty);
+        }
+
+        public static void SetScrollTo(BindableObject view, object value)
+        {
+            view.SetValue(ScrollToProperty, value);
+        }
+
+        private static void OnScrollToPropertyChanged(BindableObject bindable, object oldValue, object newValue)
+        {
+            if (!(bindable is ListView listView))
+            {
+                return;
+            }
+
+            listView.ScrollTo(newValue, ScrollToPosition.MakeVisible, true);
+        }
+    }
+}
